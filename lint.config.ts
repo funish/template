3e84469f146db24ebd9a38fcebc770e495adf1ea
Demo Macro <abc@imst.xyz>
2023-03-09 00:20:39 +0800

    ci: update configuration

--- lint.config.ts
+++ lint.config.ts
@@ -2,7 +2,7 @@ import { defineLintConfig } from "@funish/lint";
 
 export default defineLintConfig({
   staged: {
-    "*.ts": "pnpm lint:rome && pnpm lint:prettier",
-    "*.*": "pnpm prettier --check . --ignore-unknown --ignore-path .gitignore",
+    "*.ts": "pnpm check",
+    "!*.ts": "pnpm format",
   },
 });
